544e5a84a0b9553d7a48d97142a0d30a21d7fb55
Fırat ORHAN <forhan@innova.com.tr>
2022-07-31 17:03:37 +0300

    create files and initialize project

---
+++ src/components/Employee.js
@@ -0,0 +1,13 @@
+
+const Employee = () => {
+
+    return (
+        <ul>
+            <li>Employee1</li>
+            <li>Employee1</li>
+            <li>Employee1</li>
+        </ul>
+    )
+}
+
+export default Employee;
