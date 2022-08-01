d40f4ab4179b557d8ad6633a359eb1023492a6f3
Fırat ORHAN <forhan@innova.com.tr>
2022-08-01 13:24:44 +0300

    add uuid for objects and use useContext

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,12 +1,24 @@
-
-const Employee = () => {
+const Employee = ({employees}) => {
 
     return (
-        <ul>
-            <li>Employee1</li>
-            <li>Employee1</li>
-            <li>Employee1</li>
-        </ul>
+
+        <>
+        {
+            employees.map((employee) => (
+            <tr key={employee.id}>
+                <td>{employee.name}</td>
+                <td>{employee.email}</td>
+                <td>{employee.address}</td>
+                <td>{employee.phone}</td>
+                <td>
+                    <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                    <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                </td>
+            </tr>
+            ))
+        }
+         </>
+
     )
 }
 
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,27 @@
-import Employee from "./Employee";
+import { useContext } from 'react';
+import Employee from './Employee';
+import { EmployeeContext } from '../contexts/EmployeeContext';
 
 const EmployeeList = () => {
+
+    const {employees} = useContext(EmployeeContext)
+
     return (
-        <Employee />
+
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Employee employees={employees}/>
+            </tbody>
+        </table>
     )
 }
 
