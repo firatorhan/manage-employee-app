4d65bded3cc026d7c39972853499cedd7ad62ec4
Fırat ORHAN <forhan@innova.com.tr>
2022-08-03 11:55:27 +0300

    add new employee to employeeList

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,30 +1,78 @@
-import { Form, Button} from "react-bootstrap"
+import { Form, Button } from 'react-bootstrap';
+import { EmployeeContext } from '../contexts/EmployeeContext';
+import { useContext, useState } from 'react';
 
 const AddForm = () => {
 
+    const { addEmployee } = useContext(EmployeeContext);
+
+/*     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState(""); */
+
+    const [newEmployee, setNewEmployee] = useState({
+        name:"", email:"", address: "", phone: ""
+    })
+
+    const {name, email, address, phone } = newEmployee;
+
+    const onInputChange = (e) => {
+        setNewEmployee({...newEmployee, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addEmployee(name, email, address, phone)
+    }
+
     return (
-        <Form>
-            
-                <Form.Group>
-                <Form.Control typeof="text" placeholder="Name" required />
-                </Form.Group>
+        <Form onSubmit={handleSubmit}>
 
-                <Form.Group>
-                <Form.Control typeof="email" placeholder="Email" required />
-                </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    type="text"
+                    placeholder="Name *"
+                    name="name"
+                    value={name}
+                    onChange={e => onInputChange(e)}
+                    required 
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    type="email"
+                    placeholder="Email *"
+                    name="email"
+                    value={email}
+                    onChange={e => onInputChange(e)}
+                    required 
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    as="textarea"
+                    placeholder="Address *"
+                    name="address"
+                    value={address}
+                    onChange={e => onInputChange(e)}
+                    rows={3} 
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    type="text"
+                    placeholder="Phone"
+                    name="phone"
+                    value={phone}
+                    onChange={e => onInputChange(e)}
+                />
+            </Form.Group>
 
-                <Form.Group>
-                <Form.Control as="textarea" placeholder="Adress" rows={3} />
-                </Form.Group>
-
-                <Form.Group>
-                <Form.Control typeof="phone" placeholder="Phone" />
-                </Form.Group>
-
-                <Button variant = "success" typeof ="submit">Add New Employee</Button>
-            
+            <Button variant="success" type="submit" block>
+                Add New Employee
+            </Button>
         </Form>
     )
 }
-
 export default AddForm;
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,58 +1,61 @@
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import Employee from './Employee';
 import { EmployeeContext } from '../contexts/EmployeeContext';
-import { Button, Modal} from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import AddForm from './AddForm';
 
 const EmployeeList = () => {
 
-    const {employees} = useContext(EmployeeContext)
+    const { employees } = useContext(EmployeeContext)
 
     const [show, setShow] = useState(false)
 
-   const handleClose = () => setShow(false);
-   const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    useEffect(() => {
+        handleClose();
+    }, [employees])
 
     return (
         <>
-        <div className="table-title">
-              <div className="row">
-                <div className="col-sm-6">
-                  <h2>Manage <b>Employees</b></h2>
+            <div className="table-title">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h2>Manage <b>Employees</b></h2>
+                    </div>
+                    <div className="col-sm-6">
+                        <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+                    </div>
                 </div>
-                <div className="col-sm-6">
-                  <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
-                </div>
-              </div>
             </div>
-        <table className="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <Employee employees={employees}/>
-            </tbody>
-        </table>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Employee employees={employees} />
+                </tbody>
+            </table>
 
-        <Modal show={show} onHide={handleClose} > 
-             <Modal.Header className='modal-header' closeButton>
-                <Modal.Title>
-                    Add Employee
-                </Modal.Title>
-             </Modal.Header>
-             <Modal.Body>
-                <AddForm />
-             </Modal.Body>
-             <Modal.Footer>
-                <Button variant ="secondary" onClick={handleClose}>Close Modal</Button>
-             </Modal.Footer>
-        </Modal>
+            <Modal show={show} onHide={handleClose} >
+                <Modal.Header className='modal-header' closeButton>
+                    <Modal.Title>
+                        Add Employee
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AddForm />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close Modal</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
